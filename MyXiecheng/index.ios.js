2833bfb9f19ee65f7f7c5456e448260d2d8ed9ff
hoggenw <253192463@qq.com>
2017-08-12 20:58:36 +0800

    携程仿写

---
+++ MyXiecheng/index.ios.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class MyXiecheng extends Component {
+  render() {
+    return (
+      <View>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+AppRegistry.registerComponent('MyXiecheng', () => MyXiecheng);
